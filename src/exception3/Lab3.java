eee791c4c8d07c088cd50dac76b6777a4d073b79
Thitiphat <thitiphat.tana@kmutt.ac.th>
2023-03-28 14:54:37 +0700

    Exception Part 1

---
+++ src/exception3/Lab3.java
@@ -0,0 +1,19 @@
+
+package exception3;
+
+public class Lab3 {
+    public static void main(String[] args) {
+        int[] list = new int[10];
+        try {
+            System.out.println("Statement1");
+            System.out.println(list[10]);
+            System.out.println("Statement3");
+        }
+        catch (ArithmeticException e) {
+        }
+        finally {
+            System.out.println("Statement4");
+        }
+        System.out.println("Statement5");
+    }
+}
